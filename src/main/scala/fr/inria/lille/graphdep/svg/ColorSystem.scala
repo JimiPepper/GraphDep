072ec52f8dd29e049d65c113391b04bc35e80d37
Jimipepper <maitre.jiraya@live.fr>
2014-12-13 19:37:48 +0100

    Add a color system to colorize svg element

---
+++ src/main/scala/fr/inria/lille/graphdep/svg/ColorSystem.scala
@@ -0,0 +1,24 @@
+package fr.inria.lille.graphdep.svg
+
+/**
+ * Created by Jiraya on 13/12/2014.
+ */
+
+abstract class ColorSystem
+case class RGB(red : Int, green : Int, blue : Int) extends ColorSystem {
+  if(red < 0  || red > 255) {
+    throw new IllegalArgumentException("RGB values must be between 0 and 255 (red : "+ red +")")
+  }
+
+  if(green < 0  || green > 255) {
+    throw new IllegalArgumentException("RGB values must be between 0 and 255 (green : "+ green +")")
+  }
+
+  if(blue < 0  || blue > 255) {
+    throw new IllegalArgumentException("RGB values must be between 0 and 255 (blue : "+ blue +")")
+  }
+}
+
+object ColorSystem {
+
+}
