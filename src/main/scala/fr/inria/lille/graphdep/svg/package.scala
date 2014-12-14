df266325e3f3a3dd768ab769393a8f52c2637592
Jimipepper <maitre.jiraya@live.fr>
2014-12-14 01:32:16 +0100

    Add attribute to SVGElement and make them optional when you have instantiate one of them

--- src/main/scala/fr/inria/lille/graphdep/svg/package.scala
+++ src/main/scala/fr/inria/lille/graphdep/svg/package.scala
@@ -1,20 +1,20 @@
 package fr.inria.lille.graphdep
 
-import fr.inria.lille.graphdep.svg.{RGB, ColorSystem}
+import fr.inria.lille.graphdep.svg.{SVGColor, SVGStroking, RGB, ColorSystem}
 
 /**
  * @author : Romain Philippon
  */
 sealed abstract class SVGElement
-case class SVGCircle(x: Int, y : Int, fillColor : ColorSystem = RGB(255, 255, 255), stroke : SVGStroke = new SVGStroke) extends SVGElement
-case class SVGRectangle(width: Int, height: Int, x: Int, y : Int, fillColor : ColorSystem = RGB(255, 255, 255), stroke : SVGStroke = new SVGStroke) extends SVGElement
-case class SVGLine(x1 : Int, y1 : Int, x2 : Int, y2 : Int, stroke : SVGStroke = new SVGStroke) extends SVGElement
-case class SVGText(content: String, x : Int, y : Int, color : ColorSystem = RGB(255, 255, 255), font : FontSVG = new FontSVG) extends SVGElement
+case class SVGCircle(x: Int, y : Int, fillColor : ColorSystem = RGB(255, 255, 255), stroke : SVGStroke = new SVGStroke) extends SVGElement with SVGColor with SVGStroking
+case class SVGRectangle(width: Int, height: Int, x: Int, y : Int, fillColor : ColorSystem = RGB(255, 255, 255), stroke : SVGStroke = new SVGStroke) extends SVGElement with SVGColor with SVGStroking
+case class SVGLine(x1 : Int, y1 : Int, x2 : Int, y2 : Int, stroke : SVGStroke = new SVGStroke) extends SVGElement with SVGStroking
+case class SVGText(content: String, x : Int, y : Int, color : ColorSystem = RGB(255, 255, 255), font : SVGFont = new SVGFont) extends SVGElement with with SVGColor with SVGStroking
 case class SVGTitle(title : String)
 
 sealed abstract class SVGAttribute
 case class SVGStroke(color : ColorSystem = RGB(255, 255, 255), width : Int = 1, opacity : Double = 1.0) extends SVGAttribute
-case class FontSVG(size: String = "0px", weight: String = "normal")
+case class SVGFont(size: String = "0px", weight: String = "normal")
 
 
 package object svg extends SVGElementAppender {
