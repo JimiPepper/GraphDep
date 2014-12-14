98ee9e08a42ed8c6d370515312d8d01f747050b4
Jimipepper <maitre.jiraya@live.fr>
2014-12-14 04:33:12 +0100

    Add XML export (for SVG) feature

--- src/main/scala/fr/inria/lille/graphdep/svg/package.scala
+++ src/main/scala/fr/inria/lille/graphdep/svg/package.scala
@@ -1,13 +1,18 @@
 package fr.inria.lille.graphdep
 
+import java.io.File
+
 import fr.inria.lille.graphdep.svg._
+
+import scala.xml.XML
+import scala.xml.dtd.{PublicID, DocType}
 
 /**
  * @author : Romain Philippon
  */
 sealed abstract class SVGElement
 
-case class SVGCircle(x: Int, y : Int, radius : Int, fillColor : ColorSystem = RGB(255, 255, 255), stroke : SVGStroke = new SVGStroke) extends SVGElement with SVGColor with SVGStroking {
+case class SVGCircle(x: Int, y : Int, radius : Int, fillColor : ColorSystem = RGB(0, 0, 0), stroke : SVGStroke = new SVGStroke) extends SVGElement with SVGColor with SVGStroking {
   override def fillColor(color: ColorSystem): SVGElement = {
     color match {
       case RGB(red, green, blue) => SVGCircle(this.x, this.y, this.radius, color, this.stroke)
@@ -27,7 +32,7 @@ case class SVGCircle(x: Int, y : Int, radius : Int, fillColor : ColorSystem = RG
   }
 }
 
-case class SVGRectangle(width: Int, height: Int, x: Int, y : Int, fillColor : ColorSystem = RGB(255, 255, 255), stroke : SVGStroke = new SVGStroke) extends SVGElement with SVGColor with SVGStroking {
+case class SVGRectangle(width: Int, height: Int, x: Int, y : Int, fillColor : ColorSystem = RGB(0, 0, 0), stroke : SVGStroke = new SVGStroke) extends SVGElement with SVGColor with SVGStroking {
   override def fillColor(color: ColorSystem): SVGElement = {
     color match {
       case RGB(red, green, blue) => SVGRectangle(this.width, this.height, this.x, this.y, color, this.stroke)
@@ -61,7 +66,7 @@ case class SVGLine(x1 : Int, y1 : Int, x2 : Int, y2 : Int, stroke : SVGStroke = 
   }
 }
 
-case class SVGText(content: String, x : Int, y : Int, color : ColorSystem = RGB(255, 255, 255), font : SVGFont = new SVGFont) extends SVGElement with SVGColor with SVGFonting {
+case class SVGText(content: String, x : Int, y : Int, color : ColorSystem = RGB(0, 0, 0), font : SVGFont = new SVGFont) extends SVGElement with SVGColor with SVGFonting {
   override def fillColor(color: ColorSystem): SVGElement = {
     color match {
       case RGB(red, green, blue) => SVGText(this.content, this.x, this.y, RGB(red, green, blue), this.font)
@@ -79,7 +84,7 @@ case class SVGText(content: String, x : Int, y : Int, color : ColorSystem = RGB(
 case class SVGTitle(title : String)
 
 sealed abstract class SVGAttribute
-case class SVGStroke(color : ColorSystem = RGB(255, 255, 255), width : Int = 1, opacity : Double = 1.0) extends SVGAttribute
+case class SVGStroke(color : ColorSystem = RGB(0, 0, 0), width : Int = 1, opacity : Double = 1.0) extends SVGAttribute
 case class SVGFont(size: String = "0px", weight: String = "normal")
 
 
@@ -101,6 +106,27 @@ package object svg extends SVGElementAppender {
   }
 
   def export(list : Seq[SVGElement]) : Unit = {
+    val doctype = DocType(
+        "svg",
+        PublicID(
+          "-//W3C//DTD SVG 1.1//EN",
+          "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"
+        ),
+        Nil
 
+    )
+
+    val xmlSVG = <svg width="1000000px" height="100px" version="1.1" xmlns="http://www.w3.org/2000/svg">{
+      for (shape <- list) yield {
+          shape match {
+            case SVGCircle(x, y, radius, fillColor, stroke) => <circle cx={x toString} cy={y toString} r={radius toString} stroke={stroke.color.toString} stroke-width={stroke.width.toString} fill={fillColor toString} />
+            case _ =>
+          }
+        }
+      }</svg>
+
+    println(System.getProperty("user.home"))
+
+    XML.save(System.getProperty("user.home") + File.separator + "depgraph.svg", xmlSVG, "UTF-8", true, doctype)
   }
 }
