e1b83369207d6e7b6908f4de9b8b31cfcf73a66e
Jimipepper <maitre.jiraya@live.fr>
2014-12-19 05:12:57 +0100

    Fix problem with the svg document's width

--- src/main/scala/fr/inria/lille/graphdep/Main.scala
+++ src/main/scala/fr/inria/lille/graphdep/Main.scala
@@ -1,7 +1,7 @@
 package fr.inria.lille.graphdep
 
-import java.text.SimpleDateFormat
-import java.util.{Calendar, Date}
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.{LocalDate, Days}
 
 import fr.inria.lille.graphdep.svg._
 import scala.xml.XML
@@ -24,19 +24,19 @@ object Main extends App {
   var svgElements = List[SVGElement]()
   val DAY_IN_MILLIS = 1000 * 60 * 60 * 24
 
-  var currentCommitDate, commitDate, lastCommitDate : Date = null // contains the commit date of the current visited commit node while parsing
-  var parseInformation = List[(Date, Int, Int)]() // tuple <=> (commitDate, nbNewDependency, nbDeletedDependency)
+  var currentCommitDate, commitDate, lastCommitDate : LocalDate = null // contains the commit date of the current visited commit node while parsing
+  var parseInformation = List[(LocalDate, Int, Int)]() // tuple <=> (commitDate, nbNewDependency, nbDeletedDependency)
 
   /* PARSING INITIALIZATION */
   lazy val xmlFile = XML.loadFile(args(0))
   lazy val commits = xmlFile \\ "analysis" \\ "history" \\ "commit"
-  currentCommitDate = getDate(((commits head) \\ "date") text) // get first commit date
+  currentCommitDate = LocalDate.parse(((commits head) \\ "date") text) // get first commit date
   parseInformation = (currentCommitDate, 0, 0) :: parseInformation
   var nbNewAddedDependency, nbNewDeletedDependency : Int = 0
 
   /* PARSE XML RESULT FILE TO COLLECT INFORMATION DAY BY DAY */
   commits foreach { commit =>
-    commitDate = getDate((commit \\ "date").text)
+    commitDate = LocalDate.parse((commit \\ "date").text)
 
     if(sameDate(currentCommitDate, commitDate)) {
       /* UPDATE INFORMATION FOR THE DAY */
@@ -46,7 +46,7 @@ object Main extends App {
     }
     else {
       /* ADD NEW COMMIT DATE INFORMATION */
-      currentCommitDate = getDate((commit \\ "date") text)
+      currentCommitDate = LocalDate.parse((commit \\ "date") text)
       nbNewAddedDependency = (commit \\ "dependencies" \\ "added-dependency").size
       nbNewDeletedDependency = (commit \\ "dependencies" \\ "deleted-dependency").size
       addCommitInformation(currentCommitDate, nbNewAddedDependency, nbNewDeletedDependency)
@@ -57,39 +57,41 @@ object Main extends App {
   val maxAddedDependency = parseInformation.maxBy(_._2)
   val maxDeletedDependency = parseInformation.maxBy(_._3)
 
-  val widthSVG = (20 + (parseInformation.size - 1) * spaceUnitDay) // -1 because we don't need to represent the last commit datee duration
-  val heightSVG = (20 + maxAddedDependency._2 + unitGraph + + maxDeletedDependency._3 + unitGraph)
-  var middleOrdinate = heightSVG / 2
+  val heightSVG = 100 + (maxAddedDependency._2 + maxDeletedDependency._3) * unitGraph
+  val widthSVG = 20 + Days.daysBetween(parseInformation.head._1, parseInformation.last._1).getDays * spaceUnitDay
 
+  var middleOrdinate = 50 + maxAddedDependency._2 * unitGraph
   val chronoLine = line(10, middleOrdinate, widthSVG - 10, middleOrdinate)
+
   currentX = startX
   lastCommitDate = parseInformation.head._1
   var addDepLine, delDepLine, commitDateCirc, nbAddDeptext, nbDelDeptext : SVGElement = null
 
-  parseInformation foreach { case (date : Date, nbAddedDependency : Int, nbDeletedDependency : Int) =>
+  parseInformation foreach { case (date : LocalDate, nbAddedDependency : Int, nbDeletedDependency : Int) =>
     /* MOVE VIRTUAL DRAWING CURSOR */
-    nbDays = ((date.getTime() - lastCommitDate.getTime())/ DAY_IN_MILLIS ).toInt
+    nbDays = Days.daysBetween(lastCommitDate, date).getDays
     currentX += nbDays * spaceUnitDay
     lastCommitDate = date
 
     /* CREATE SVG ELEMENTS */
-    if(nbAddedDependency == 0 && nbDeletedDependency == 0) // if no dependency changes detected
+    if(nbAddedDependency == 0 && nbDeletedDependency == 0) { // if no dependency changes detected
       commitDateCirc = circle(currentX, middleOrdinate, 5).fillColor(RGB(255, 255, 255))
+    }
     else {
       commitDateCirc = circle(currentX, middleOrdinate, 5)
 
       if (nbAddedDependency > 0) {
-        nbAddDeptext = text("+" + nbAddedDependency, currentX, middleOrdinate - nbAddedDependency * unitGraph - 5).fillColor(newDependencyColor)
-        addDepLine = line(currentX, middleOrdinate, currentX, middleOrdinate - nbAddedDependency * unitGraph).strokeColor(newDependencyColor)
+        nbAddDeptext = text("+" + nbAddedDependency, currentX, middleOrdinate - (nbAddedDependency * unitGraph) - 5).fillColor(newDependencyColor)
+        addDepLine = line(currentX, middleOrdinate, currentX, middleOrdinate - (nbAddedDependency * unitGraph)).strokeColor(newDependencyColor)
         svgElements = svgElements.:+(addDepLine)
       }
       else {
-        nbAddDeptext = text("0", currentX, middleOrdinate - nbAddedDependency * unitGraph - 5).fillColor(newDependencyColor)
+        nbAddDeptext = text("0", currentX, middleOrdinate - (nbAddedDependency * unitGraph) - 5).fillColor(newDependencyColor)
       }
 
       if(nbDeletedDependency > 0) {
-        nbDelDeptext = text("-" + nbDeletedDependency, currentX, middleOrdinate + nbDeletedDependency * unitGraph + 12 + 5).fillColor(deleteDependencyColor)
-        delDepLine = line(currentX, middleOrdinate, currentX, middleOrdinate + nbDeletedDependency * unitGraph).strokeColor(deleteDependencyColor)
+        nbDelDeptext = text("-" + nbDeletedDependency, currentX, middleOrdinate + (nbDeletedDependency * unitGraph) + 12 + 5).fillColor(deleteDependencyColor)
+        delDepLine = line(currentX, middleOrdinate, currentX, middleOrdinate + (nbDeletedDependency * unitGraph)).strokeColor(deleteDependencyColor)
         svgElements = svgElements.:+(delDepLine)
       }
       else {
@@ -111,13 +113,6 @@ object Main extends App {
   export(widthSVG, heightSVG, svgElements)
 
   /**
-   * Returns an Date object from a string representation
-   * @param date
-   * @return a date object
-   */
-  def getDate(date : String) = { val format = new SimpleDateFormat("yyyy-MM-dd") ; format.parse(date) }
-
-  /**
    * Updates the the commit day information node in parseInformation variable
    * @param nbAddedDependency is the number of new added dependencies
    * @param nbDeletedDependency is the number of new deleted dependencies
@@ -132,7 +127,7 @@ object Main extends App {
    * @param nbAddedDependency is the number of new added dependencies
    * @param nbDeletedDependency is the number of new deleted dependencies
    */
-  def addCommitInformation(date : Date, nbAddedDependency : Int, nbDeletedDependency : Int) : Unit = {
+  def addCommitInformation(date : LocalDate, nbAddedDependency : Int, nbDeletedDependency : Int) : Unit = {
     parseInformation = parseInformation.:+(currentCommitDate, nbNewAddedDependency, nbNewDeletedDependency)
   }
 
@@ -142,13 +137,5 @@ object Main extends App {
    * @param date2 is the second date that must be compare to date1
    * @return true if the two dates are the same, else otherwise
    */
-  def sameDate(date1 : Date, date2 : Date) : Boolean = {
-    val cal1 : Calendar = Calendar.getInstance()
-    val cal2 : Calendar = Calendar.getInstance()
-
-    cal1.setTime(date1)
-    cal2.setTime(date2)
-
-    cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) && cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH) && cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR)
-  }
+  def sameDate(date1 : LocalDate, date2 : LocalDate) : Boolean = { date1.equals(date2) }
 }
